354d9d07114934ef267ccc61fcbc795fc565ba72
Irlene Galiza <irleneviturino@gmail.com>
2021-07-09 17:22:47 +0100

    first commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Certificard - aula10
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/irlene-viturino/pen/yLgPOVO](https://codepen.io/irlene-viturino/pen/yLgPOVO).
+
+
---
+++ src/script.js
@@ -0,0 +1,63 @@
+/*
+CERTIFICARD
+PARTICIPE E VÁ MAIS FUNDO
+Nesta últiima aula da Imersão Dev, vamos focar exclusivamente no HTML e CSS e criar nosso próprio certificado do zero.
+
+
+Conteúdo detalhado dessa aula
+Criamos um certificado do zero;
+Adicionamos o HTML para estruturar nossa página;
+Estilizamos a página com CSS do zero.
+
+Desafios dessa aula!
+Adicionar um link em cada linha para abrir o projeto no CodePen.
+
+Links importantes para você acompanhar a aula
+Codepen - editor de código online
+Canal do Mario Souto no Youtube
+Canal da Rafa no Youtube
+Canal do Gui Lima no Youtube
+
+Links citados nessa aula
+Gerador de Box Shadow(https://html-css-js.com/css/generator/box-shadow/)
+Referências de CSS(https://developer.mozilla.org/pt-BR/docs/Web/CSS/Reference)
+
+Conteúdos extras
+Código final da aula 9(https://codepen.io/imersao-dev/pen/dyNyyLa)
+Código final da aula 10(https://codepen.io/imersao-dev/pen/QWdGWYv)
+JavaScript segundo a documentação(https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
+Primeiros passos na programação(https://hipsters.tech/primeiros-passos-na-programacao-a-imersao-dev-hipsters-ponto-tech-243/)
+5 minutos de HTML(https://www.youtube.com/watch?v=3oSIqIqzN3M)
+Como abrir um projeto do CodePen no VSCode(https://www.youtube.com/watch?v=xvkuNF_8Coc)
+Dicas de Programação(https://www.youtube.com/watch?v=x3-s0HidA04&list=PLTcmLKdIkOWkJY8LJXs1GDqBnxCGsExBO)
+Figma e AluraFlix(https://www.youtube.com/watch?v=nHI0rQIw5Z8&list=PLirko8T4cEmwudNmVUWT-JbQ5FAKkXGsl)
+
+*/
+
+/*Próximos passos
+-Inteligência Artificial, Machine Learning, Data Science e Deep Learning(https://www.youtube.com/watch?v=ccZ2pyr3YDw&list=PLMdYygf53DP7YZiFUtGTWJJlvynRyrna-)
+-Flutter(https://www.youtube.com/watch?v=J4BVaXkwmM8)
+-Frontend ou Backend com a Rafa(https://www.youtube.com/watch?v=dp0zB4n3MUs&list=PLhkO7OMKgT_oXQtrM2JjR-AzH0-v4kRzn)
+-Programação e Tecnologia(https://www.youtube.com/watch?v=R8of-SVzxJU&list=PLhkO7OMKgT_qEXMteR9O0M3DyKq1OvC_h)
+-Frontend(https://www.youtube.com/watch?v=Em0R3csNMVE&list=PLhkO7OMKgT_p3VoB47VHdLGpYTzxYTv8R)
+-Projetos para praticar JS(https://www.youtube.com/watch?v=4W55nFDyIrc&list=PLTcmLKdIkOWntJJVhd0etFmdN8bWXJif9)
+-Sucesso é sorte ou esforço?(https://www.youtube.com/watch?v=yLFQtqEqj0M&list=PL-kOa62ayb1yb4WMWID2t9ohnGjXLr8Jy)
+-Como estudar melhor?(https://www.youtube.com/watch?v=UTHMKSxLHsI&list=PL-kOa62ayb1wQPeBChJ6KBILRLL2AHoZU)
+
+sharelinkgenerator para criar links para fazer ligações no seu portfolio
+https://html-css-js.com/css/generator/box-shadow/
+https://emojipedia.org/student-medium-skin-tone/
+https://developer.mozilla.org/pt-BR//
+
+Como compartilhar seu CodePen
+Clique na opção Settings no canto superior direito;
+Selecione a opção Pen Details (Detalhes);
+No campo Pen Title você pode dar o nome do seu projeto;
+No campo Pen Description, você pode dar detalhes sobre o projeto (o que ele faz, qual objetivo do projeto);
+Para compartilhar seu projeto, no campo Tags, você pode adicionar #imersaodev,#alura
+Agora só salvar seu projeto e compartilhar o link marcando a Alura nas redes sociais!
+
+*/
+
+/* Live de Abertura do Imersão DEV (https://www.youtube.com/watch?v=P3Q9hSc0vl8)
+Live de Encerramento do Imersão DEV (https://www.youtube.com/watch?v=W-QkHsPZJs4)*/
